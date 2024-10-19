77fda4764099405943f51726acf6129c34efc7fd
Nikhil Choudhary <nikhilji7974@gmail.com>
2024-10-19 16:03:26 +0530

    atlasdbconnection

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mongooseUri = process.env.MONGO_URL; // Local MongoDB URI
 const mongoosrUriAtlas = process.env.MONGO_URL_ATLAS; // MongoDB Atlas URI
 
 // Connect to MongoDB Atlas
-mongoose.connect(mongooseUri, {
+mongoose.connect(mongoosrUriAtlas, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });

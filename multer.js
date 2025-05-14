c2516bb88f01b278d8cfe6a31607f8d2fa597116
Nikhil Choudhary <nikhilji7974@gmail.com>
2025-05-14 17:05:49 +0530

    corrected the naming convenction of the images

--- multer.js
+++ multer.js
@@ -1,20 +1,19 @@
-// 
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('./cloudinaryConfig'); // Adjust path as necessary
-const path = require('path')
-// Configure Cloudinary Storage for multer
+
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-        folder: 'uploads', // Optional: Define a folder in Cloudinary
-        allowed_formats: ['jpg', 'png', 'jpeg', 'gif','webp'],
-        filename: (req, file) => {
-            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-            return `${file.fieldname}-${uniqueSuffix}${path.extname(file.originalname)}`;
-        },
+  cloudinary: cloudinary,
+  params: {
+    folder: 'uploads',
+    allowed_formats: ['jpg', 'png', 'jpeg', 'gif', 'webp'],
+    public_id: (req, file) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      return `${file.fieldname}-${uniqueSuffix}`;
     },
+  },
 });
-const upload = multer({ storage: storage });
+
+const upload = multer({ storage });
 
 module.exports = upload;
